0e0cd52b56d6ca66f7eb0bba6a4986fb2ec1ae85
anonymous anonymous <cat.knoerr@kellogg.northwestern.edu>
2021-11-15 18:11:09 +0000

    Generate graphiti resources for directors

---
+++ app/resources/director_resource.rb
@@ -0,0 +1,15 @@
+class DirectorResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :dob, :string
+  attribute :bio, :string
+  attribute :image, :string
+  attribute :films, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
