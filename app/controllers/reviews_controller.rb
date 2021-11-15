fad46422d637c055cee7691f6ed1d928ba3092f5
anonymous anonymous <cat.knoerr@kellogg.northwestern.edu>
2021-11-15 18:11:55 +0000

    Remove controller comments

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,19 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: %i[show edit update destroy]
 
-  # GET /reviews
   def index
     @q = Review.ransack(params[:q])
     @reviews = @q.result(distinct: true).includes(:user).page(params[:page]).per(10)
   end
 
-  # GET /reviews/1
   def show; end
 
-  # GET /reviews/new
   def new
     @review = Review.new
   end
 
-  # GET /reviews/1/edit
   def edit; end
 
-  # POST /reviews
   def create
     @review = Review.new(review_params)
 
@@ -34,7 +29,6 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reviews/1
   def update
     if @review.update(review_params)
       redirect_to @review, notice: "Review was successfully updated."
@@ -43,7 +37,6 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # DELETE /reviews/1
   def destroy
     @review.destroy
     message = "Review was successfully deleted."
@@ -56,12 +49,10 @@ class ReviewsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_review
     @review = Review.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def review_params
     params.require(:review).permit(:review, :rating, :user_id, :date)
   end
