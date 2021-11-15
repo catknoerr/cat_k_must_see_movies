36c908d82e6bd877174cd1e5342b02f02d5bc43b
anonymous anonymous <cat.knoerr@kellogg.northwestern.edu>
2021-11-15 18:11:07 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,18 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :year, :integer
+  attribute :duration, :integer
+  attribute :description, :string
+  attribute :image, :string
+  attribute :director_id, :integer
+  attribute :bookmark_id, :integer
+  attribute :review_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
